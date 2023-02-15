b27b67a22f81629b2650b5bce5d728ad23b9458f
Samurai_Yuk <101939251+Vege-dog@users.noreply.github.com>
2023-02-15 23:54:57 +0800

    Add files via upload

--- chapter_2.c
+++ chapter_2.c
@@ -1,7 +1,17 @@
-#include studio
-//2.3甯搁噺
-#define VTAB'\013'
-#define BELL'\007'
+#include<stdio.h>
 
-#define VTAB'\xb'
-#define BELL'\x7'
+//2.2数据类型及长度
+#include<limits.h>
+
+//确定类型的取值范围
+main() {
+	//带符号
+	printf("signed char min= %d\n", SCHAR_MIN);
+	printf("signed char max= %d\n", SCHAR_MAX);
+	printf("signed short min= %d\n", SHRT_MIN);
+	printf("signed short max= %d\n", SHRT_MAX);
+	printf("signed int min= %d\n", INT_MIN);
+	printf("signed int max= %d\n", INT_MAX);
+	printf("signed long min= %d\n", LONG_MIN);
+	printf("signed long max= %d\n", LONG_MAX);
+}
